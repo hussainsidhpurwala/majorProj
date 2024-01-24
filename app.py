8789f513bf6bb695a1a729f5695ad3cc373508ef
hussainsidhpurwala <hussain.612059.it@mhssce.ac.in>
2024-01-24 12:47:11 +0530

    second commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def dashboard():
     return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 app.run(debug=True)
